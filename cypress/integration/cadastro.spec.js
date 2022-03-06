9392654ada4fde7556b2a62b7140ce9244fc8894
vscalado <vscalado@padtec.com.br>
2022-03-06 12:27:30 -0300

    Adicionando primeiro teste

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,21 @@
+describe('Cadastro', ()=> {
+	it('Usuário deve se tornar um entregador', ()=> {
+		cy.viewport(1440, 900 );
+		cy.visit('https://buger-eats.vercel.app');
+		
+		cy.get('a[href="/deliver"]').click();
+		cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas'); //ChekcPoint
+
+		var entregador = {
+			nome: 'Fernando Papito',
+			cpf: '00000014141',
+			email: 'papito@hotmail.com',
+			whatsapp: 11999999999
+		}
+
+		cy.get('input[name="name"]').type(entregador.nome);
+		cy.get('input[name="cpf"]').type(entregador.cpf);
+		cy.get('input[name="email"]').type(entregador.email);
+		cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+	});
+});
